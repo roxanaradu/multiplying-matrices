c6d7668b39e1f0eb9f380b2a3761010374cd3931
Roxana Radu <roxana_radu95@yahoo.com>
2015-06-23 15:46:50 +0300

    Added comments

--- multiply.c
+++ multiply.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 int main()
 {
-///////
+
+	/*Declarearea variabilelor */
 	int a[100][100],b[100][100],c[100][100];
 	FILE *fin;
 	fin=fopen("matrice.in","r");
-	int i,j;
+	int i,j,k;
 	int m1,n1,m2,n2;
+
+	/*Citirea datelor de intrare */  
+
 	fscanf(fin,"%d %d ",&m1,&n1);
 	for ( i=0;i<m1;i++)
 		for(j=0;j<n1;j++)
@@ -17,13 +21,14 @@ int main()
 		for(j=0;j<n2;j++)
 			fscanf(fin,"%d",&b[i][j]);
 
+	/*Testare compatibilitate inmultire */
 	if( n1!=m2)
 	{
 		printf("Matricele nu se pot inmulti");
 		return -1;
 	}
 	
-	int k;
+	/*Algoritmul de inmultire*/
 	for ( i=0;i<m1;i++)
 		for(j=0;j<n2;j++)
 		{
@@ -32,6 +37,8 @@ int main()
 				sum=sum+a[i][k]*b[k][j];
 			c[i][j]=sum;
 		}
+
+	/*Afisare rezultat */
 	printf("Afisarea matricei rezultat:\n");
 	for( i=0;i<m1;i++)
 	{
