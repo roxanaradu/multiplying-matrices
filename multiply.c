02d3acbd2d50ec7a937f2c5a3fe1202db9a5f639
constantin nastase <ctin.nastase5@gmail.com>
2015-06-23 15:27:12 +0300

    source added

---
+++ multiply.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+	int a[100][100],b[100][100],c[100][100];
+	FILE *fin;
+	fin=fopen("matrice.in","r");
+	int i,j;
+	int m1,n1,m2,n2;
+	fscanf(fin,"%d %d ",&m1,&n1);
+	for ( i=0;i<m1;i++)
+		for(j=0;j<n1;j++)
+			fscanf(fin,"%d",&a[i][j]);
+	
+	fscanf(fin,"%d %d",&m2,&n2);
+	for ( i=0;i<m2;i++)
+		for(j=0;j<n2;j++)
+			fscanf(fin,"%d",&b[i][j]);
+
+	if( n1!=m2)
+	{
+		printf("Matricele nu se pot inmulti");
+		return -1;
+	}
+	
+	int k;
+	for ( i=0;i<m1;i++)
+		for(j=0;j<n2;j++)
+		{
+			int sum=0;
+			for(k=0;k<n1;k++)
+				sum=sum+a[i][k]*b[k][j];
+			c[i][j]=sum;
+		}
+	printf("Afisarea matricei rezultat:\n");
+	for( i=0;i<m1;i++)
+	{
+		for(j=0;j<n2;j++)
+			printf("%d  ",c[i][j]);
+		printf("\n");
+	}
+}	
+			
+
